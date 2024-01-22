f9757efded056903ccb0ae235d827a33c4a78661
Manu Sartor <mimostejidos@gmail.com>
2024-01-22 00:20:58 -0300

    very rought draft

--- wovenPlacemats.js
+++ wovenPlacemats.js
@@ -6,54 +6,17 @@ let YCselectionButtons;
 let YCparentDiv;
 let YCultimateParent;
 
-let YCbuttonValue
+// let YCbuttons
+let userSelection
+let YCbuttonName
 let firedButtonID;
 let firedButton;
+let selectedYarnColors;
 
+let readValuesBtn;
 
-let pair1Left;
-let pair1Right;
-let pair2Left;
-let pair2Right;
-let pair3Left;
-let pair3Right;
-let pair4Left;
-let pair4Right;
-let pair5Left;
-let pair5Right;
-let pair6Left;
-let pair6Right;
-let pair7Left;
-let pair7Right;
-let pair8Left;
-let pair8Right;
-let pair9Left;
-let pair9Right;
-let pair10Left;
-let pair10Right;
-let pair11Left;
-let pair11Right;
-let pair12Left;
-let pair12Right;
-let pair13Left;
-let pair13Right;
-let pair14Left;
-let pair14Right;
-
-let pair1LeftYC;
-let pair1RightYC;
-let pair2LeftYC;
-let pair2RightYC;
-
-let pair1LeftMC;
-let pair1RightMC;
-let pair2LeftMC;
-let pair2RightMC;
-
-let pair1LeftCC;
-let pair1RightCC;
-let pair2LeftCC;
-let pair2RightCC;
+let outputDiv;
+let getSetUpRowsBtn;
 
 window.onload = init();
 
@@ -65,75 +28,126 @@ function init() {
 
 function getDOMelements () {
     console.log('function getDOMelements executed');
-    // pair1Left = document.querySelector('#pair1Left');
-    // pair1Right = document.querySelector('#pair1Right');
-    // pair2Left = document.querySelector('#pair2Left');
-    // pair2Right = document.querySelector('#pair2Right');
-
-    // pair1LeftMC = document.querySelector('#pair1LeftMC');
-    // pair1RightMC = document.querySelector('#pair1RightMC');
-    // pair2LeftMC = document.querySelector('#pair2LeftMC');
-    // pair2RightMC = document.querySelector('#pair2RightMC');
-
-    // pair1LeftCC = document.querySelector('#pair1LeftCC');
-    // pair1RightCC = document.querySelector('#pair1RightCC');
-    // pair2LeftCC = document.querySelector('#pair2LeftCC');
-    // pair2RightCC = document.querySelector('#pair2RightCC');
-
     YCselectionButtons = document.querySelectorAll('.YCselection');
-
+    outputDiv = document.querySelector('#outputDiv')
+    getSetUpRowsBtn = document.querySelector('#getSetUpRowsBtn')
+    readValuesBtn = document.querySelector('#readValues');
     addEventListeners ();
+    getYCforEachBtn();
 }
 
 function addEventListeners () {
     console.log('function addEventListeners executed');
-    // pair1LeftMC.addEventListener('click', selectOption);
-    // pair1RightMC.addEventListener('click', selectOption);
-    // pair1LeftCC.addEventListener('click', selectOption);
-    // pair1RightCC.addEventListener('click', selectOption);
-    // pair2Left.addEventListener('change', getValues);
-    // pair2Right.addEventListener('select', getValues);
     YCselectionButtons = document.querySelectorAll('.YCselection');
     console.log('YCselectionButtons.length = ' + YCselectionButtons.length);
      for (let i = 0; i < YCselectionButtons.length; i++) {
-        // console.log(YCselectionButtons[i]);
-        // YCselectionButtons[i].addEventListener('click', getYC);
-        YCselectionButtons[i].addEventListener('click', function(event) {getYC(YCselectionButtons[i])});
-
-
+        YCselectionButtons[i].addEventListener('click', function(event) {writeYC(YCselectionButtons[i])});
      }
+    getSetUpRowsBtn.addEventListener('click', writeYC);
+    readValuesBtn.addEventListener('click', readValues);
 }
 
-function getYC (YCbutton) {
-    console.log('getYC function executed');
+function writeYC (YCbutton) {
+    console.log('writeYC function executed');
     console.log('event.target.id: ' + event.target.id);
     firedButtonID = event.target.id;
     firedButton = document.querySelector('#'+firedButtonID);
     firedButton.classList.add('selected');
-    console.log(YCbutton);
-    console.log('YCbutton.parentElement');
-    console.log(YCbutton.parentElement);
     YCparentDiv = YCbutton.parentElement;
-    // console.log('YCparentDiv');
-    // console.log(YCparentDiv);
     YCultimateParent = YCparentDiv.parentElement;
-    // console.log('YCultimateParent');
-    // console.log(YCultimateParent);
-        //   console.log('changing innerHTML of: ');
-        // console.log( YCultimateParent );    
         YCultimateParent.innerHTML = event.target.innerHTML;
         YCultimateParent.classList.add('strong');
-        // console.log(YCparentDiv)
-        let x = YCultimateParent.innerHTML;
-        console.log(x)
-        console.log(YCbutton)
-        YCbuttonValue = YCbutton.id;
-        console.log(YCbuttonValue)
-        YCbutton.setAttribute('value', YCbuttonValue)
-        console.log(YCbutton.target)
+        let defineColor = YCultimateParent.innerHTML;
+        console.log('defineColor: ' + defineColor);
+        YCbuttonName = YCbutton.id;
+        YCbutton.setAttribute('value', YCbuttonName);
+    for (let i = 0; i < YCselectionButtons.length; i++) {
+        if (YCselectionButtons[i].id == YCbuttonName) {
+            console.log('YCselectionButtons[i] = YCselectionButtons[' + i +']');
+            console.log(YCselectionButtons[i]);
+            console.log('YCselectionButtons[i].id:' + YCselectionButtons[i].id);
+            console.log('YCbuttonName: ' + YCbuttonName);
+            console.log('YCselectionButtons[i].innerHTML: ' + YCselectionButtons[i].innerHTML);
+        }
+    }
+}
+
+function getPairNumber () {
+
+}
+
+function defineLeftRight () {
+    console.log('defineLeftRight function executed');
+    for (let i = 0; i < YCselectionButtons.length; i++) {
+        for (let j = 0; j < YCselectionButtons[i].classList.length; j++) {
+            if (YCselectionButtons[i].classList[j] == 'left') {
+                YCselectionButtons[i].direction = 'left';
+                // console.log('YCselectionButtons[' + [i] + '] is ' + YCselectionButtons[i].direction);
+                console.log('---left---');
+            } else if (YCselectionButtons[i].classList[j] == 'right') {
+                YCselectionButtons[i].direction = 'right';
+                // console.log('YCselectionButtons[' + [i] + '] is ' + YCselectionButtons[i].direction);
+                console.log('---right---');
+            }
+        }
+    }   
+}
+
+function getYCforEachBtn () {
+    console.log('getYCforEachBtn function executed') //not working properly
+    for (let i = 0; i < YCselectionButtons.length; i++) {
+        for (let j = 0; j < YCselectionButtons[i].classList.length; j++) {
+            if (YCselectionButtons[i].classList[j] == 'MC') {
+                YCselectionButtons[i].yarnColor = 'MC';
+                // console.log('YCselectionButtons[' + [i] + '] is ' + YCselectionButtons[i].yarnColor);
+            } else if (YCselectionButtons[i].classList[j] == 'CC') {
+                YCselectionButtons[i].yarnColor = 'CC';
+                // console.log('YCselectionButtons[' + [i] + '] is ' + YCselectionButtons[i].yarnColor);
+            }
+        }
+    }
+}
+
+function defineYarnColor () { //keep working on this!!!
+    console.log('defineYarnColor function executed');
+    for (let i = 0; i < YCselectionButtons.length; i++) {
+        // YCselectionButtons[i]. = YCselectionButtons[i].parentElement.innerHTML;
+        console.log('YCselectionButtons[i].parentElement.innerHTML (userSelection):');
+        console.log(YCselectionButtons[i].parentElement.innerHTML);
+        userSelection = YCselectionButtons[i].parentElement.innerHTML;
+        console.log('userSelection: ');
+        console.log(userSelection);
+        // if (YCselectionButtons[i].parentElement.classList == YCselectionButtons[i].yarnColor) {
+            for (let j = 0; j < YCselectionButtons[i].parentElement.classList.length; j++) {
+                if (userSelection.classList[j] == 'selected') {
+                    console.log('YCselectionButtons[' + i + '] =' + YCselectionButtons[i].yarnColor)
+                    console.log('match')
+            }
+            
+        }
+        // if (YCselectionButtons[i].yarnColor == YCselectionButtons[i].parentElement.innerHTML) {
+            
+        // }
+        // for (let k = 0; k < YCselectionButtons[i].parentElement) {
+        // if (YCselectionButtons[i].yarnColor == YCselectionButtons[i].innerHTML) {
+        //     console.log('MATCH!' + i);
+        //     console.log('YCselectionButtons[i].yarnColor: ' + YCselectionButtons[i].yarnColor);
+        //     console.log('YCselectionButtons[i].innerHTML: ' + YCselectionButtons[i].innerHTML)
+        //     console.log('YCselectionButtons[i].id: ' + YCselectionButtons[i].id);
+        //     // console.log(YCselectionButtons[i] + ' = ' + YCbuttonName);
+        // }
+    }
     
+}
 
-
-
-
+function readValues () {
+    defineYarnColor();
+    defineLeftRight();
+    console.log('readValues function executed')
+    for (let i = 0; i < YCselectionButtons.length; i++) {
+        console.log('YCselectionButtons[i] = ' + 'YCselectionButtons[' + i + ']' );
+        console.log('YCbuttonName: ' + YCbuttonName);
+        console.log('YCselectionButtons[i].direction: ' + YCselectionButtons[i].direction);
+        console.log('YCselectionButtons[i].yarnColor: ' + YCselectionButtons[i].yarnColor );
+    }
 }
